d24dfbb38ea424fb4dac82ff08b38695cee69a3e
Aneesh Maganti <28660350+aminoa@users.noreply.github.com>
2024-02-13 15:19:44 -0500

    two objects on the screen

---
+++ Project 1/src/main.cpp
@@ -0,0 +1,259 @@
+/**
+* Author: Aneesh Maganti
+* Assignment: Simple 2D Scene
+* Date due: 2024-02-17, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+#define GL_SILENCE_DEPRECATION
+#define GL_GLEXT_PROTOTYPES 1
+#define LOG(argument) std::cout << argument << '\n'
+
+#include <iostream>
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include "ShaderProgram.h"               // We'll talk about these later in the course
+
+#include "glm/mat4x4.hpp"                // 4x4 Matrix
+#include "glm/gtc/matrix_transform.hpp"  // Matrix transformation methods
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+enum Coordinate
+{
+    x_coordinate,
+    y_coordinate
+};
+
+#define LOG(argument) std::cout << argument << '\n'
+
+const int WINDOW_WIDTH = 640,
+WINDOW_HEIGHT = 480;
+
+const float BG_RED = 0.1922f,
+BG_BLUE = 0.549f,
+BG_GREEN = 0.9059f,
+BG_OPACITY = 1.0f;
+
+const int VIEWPORT_X = 0,
+VIEWPORT_Y = 0,
+VIEWPORT_WIDTH = WINDOW_WIDTH,
+VIEWPORT_HEIGHT = WINDOW_HEIGHT;
+
+const char V_SHADER_PATH[] = "./src/shaders/vertex_textured.glsl",
+F_SHADER_PATH[] = "./src/shaders/fragment_textured.glsl";
+
+const float MILLISECONDS_IN_SECOND = 1000.0;
+const float DEGREES_PER_SECOND = 90.0f;
+
+const int NUMBER_OF_TEXTURES = 1; // to be generated, that is
+const GLint LEVEL_OF_DETAIL = 0;  // base image level; Level n is the nth mipmap reduction image
+const GLint TEXTURE_BORDER = 0;   // this value MUST be zero
+
+const char OBJECT1_SPRITE_FILEPATH[] = "./assets/tux.png";
+const char OBJECT2_SPRITE_FILEPATH[] = "./assets/daemon.png";
+
+SDL_Window* g_display_window;
+bool g_game_is_running = true;
+bool g_is_growing = true;
+
+ShaderProgram g_shader_program;
+glm::mat4 view_matrix; // Defines the position (location and orientation) of the camera;
+glm::mat4 g_object1_model_matrix; // Defines every translation, rotation, and/or scaling applied to an object; we'll look at these next week
+glm::mat4 g_object2_model_matrix; 
+
+glm::mat4 g_projection_matrix; // Defines the characteristics of your camera, such as clip panes, field of view, projection method, etc.
+glm::mat4 g_trans_matrix;
+
+float g_previous_ticks = 0.0f;
+
+GLuint g_object1_texture_id;
+GLuint g_object2_texture_id;
+SDL_Joystick* g_player_one_controller;
+
+glm::vec3 g_object1_position = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_object1_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+
+glm::vec3 g_object2_position = glm::vec3(2.0f, 0.0f, 0.0f);
+glm::vec3 g_object2_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+
+float get_screen_to_ortho(float coordinate, Coordinate axis)
+{
+    switch (axis) {
+    case x_coordinate:
+        return ((coordinate / WINDOW_WIDTH) * 10.0f) - (10.0f / 2.0f);
+    case y_coordinate:
+        return (((WINDOW_HEIGHT - coordinate) / WINDOW_HEIGHT) * 7.5f) - (7.5f / 2.0);
+    default:
+        return 0.0f;
+    }
+}
+
+GLuint load_texture(const char* filepath)
+{
+    // STEP 1: Loading the image file
+    int width, height, number_of_components;
+    unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
+
+    if (image == NULL)
+    {
+        LOG("Unable to load image. Make sure the path is correct.");
+        LOG(filepath);
+        assert(false);
+    }
+
+    // STEP 2: Generating and binding a texture ID to our image
+    GLuint textureID;
+    glGenTextures(NUMBER_OF_TEXTURES, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    // STEP 3: Setting our texture filter parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    // STEP 4: Releasing our file from memory and returning our texture id
+    stbi_image_free(image);
+
+    return textureID;
+}
+
+void initialise()
+{
+    // Initialise video and joystick subsystems
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
+
+    // Open the first controller found. Returns null on error
+    g_player_one_controller = SDL_JoystickOpen(0);
+
+    g_display_window = SDL_CreateWindow("Project 1",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_OPENGL);
+
+    SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
+    SDL_GL_MakeCurrent(g_display_window, context);
+
+    glewInit();
+
+    glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+
+    g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
+
+    g_object1_model_matrix = glm::mat4(1.0f);
+    g_object2_model_matrix = glm::mat4(1.0f);
+
+    view_matrix = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
+    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
+
+    g_shader_program.set_projection_matrix(g_projection_matrix);
+    g_shader_program.set_view_matrix(view_matrix);
+    // Notice we haven't set our model matrix yet!
+
+    glUseProgram(g_shader_program.get_program_id());
+
+    glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
+
+    g_object1_texture_id = load_texture(OBJECT1_SPRITE_FILEPATH);
+    g_object2_texture_id = load_texture(OBJECT2_SPRITE_FILEPATH);
+
+    // enable blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void update()
+{
+    float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND; // get the current number of ticks
+    float delta_time = ticks - g_previous_ticks; // the delta time is the difference from the last frame
+    g_previous_ticks = ticks;
+
+    g_object1_position += g_object1_movement * delta_time * 1.0f;
+    g_object2_position += g_object2_movement * delta_time * 1.0f;
+
+    g_object1_model_matrix = glm::mat4(1.0f);
+    g_object1_model_matrix = glm::translate(g_object1_model_matrix, g_object1_position);
+
+    g_object2_model_matrix = glm::mat4(1.0f);
+    g_object2_model_matrix = glm::translate(g_object2_model_matrix, g_object2_position);
+}
+
+void draw_object(glm::mat4& object_model_matrix, GLuint& object_texture_id)
+{
+    g_shader_program.set_model_matrix(object_model_matrix);
+    glBindTexture(GL_TEXTURE_2D, object_texture_id);
+    glDrawArrays(GL_TRIANGLES, 0, 6); // we are now drawing 2 triangles, so we use 6 instead of 3
+}
+
+void render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Vertices
+    float object1_vertices[] = {
+        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
+        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
+    };
+
+    // Textures
+    float object1_texture_coordinates[] = {
+        0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
+        0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
+    };
+
+    // Vertices
+    float object2_vertices[] = {
+        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
+        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
+    };
+
+    // Textures
+    float object2_texture_coordinates[] = {
+        0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
+        0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
+    };
+
+
+    glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false, 0, object1_vertices);
+    glEnableVertexAttribArray(g_shader_program.get_position_attribute());
+
+    glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, object2_texture_coordinates);
+    glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+
+    // Bind texture
+    draw_object(g_object1_model_matrix, g_object1_texture_id);
+    draw_object(g_object2_model_matrix, g_object2_texture_id);
+
+    // We disable two attribute arrays now
+    glDisableVertexAttribArray(g_shader_program.get_position_attribute());
+    glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+
+    SDL_GL_SwapWindow(g_display_window);
+}
+
+void shutdown()
+{
+    SDL_JoystickClose(g_player_one_controller);
+    SDL_Quit();
+}
+
+/**
+ Start here—we can see the general structure of a game loop without worrying too much about the details yet.
+ */
+int main(int argc, char* argv[])
+{
+    initialise();
+
+    while (g_game_is_running)
+    {
+        update();
+        render();
+    }
+
+    shutdown();
+    return 0;
+}
