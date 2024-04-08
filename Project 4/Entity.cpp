2d2c6470c4e2f4cfd542013eea8aaa4015afa3bd
Aneesh Maganti <28660350+aminoa@users.noreply.github.com>
2024-04-08 11:57:31 -0400

    project 5 init

--- Project 4/Entity.cpp
+++ Project 4/Entity.cpp
@@ -1,3 +1,12 @@
+/**
+* Author: Aneesh Maganti
+* Assignment: Rise of the AI
+* Date due: 2024-03-23, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 
