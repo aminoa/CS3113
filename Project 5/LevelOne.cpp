f260d09985bb993f96b4cf0839fc5151361bb0bd
Aneesh Maganti <28660350+aminoa@users.noreply.github.com>
2024-04-10 11:22:09 -0400

    got level 1 done

--- Project 5/LevelOne.cpp
+++ Project 5/LevelOne.cpp
@@ -4,23 +4,34 @@
 #define LEVEL_WIDTH 14
 #define LEVEL_HEIGHT 8
 
+const float BG_RED = 0.4f,
+BG_BLUE = 0.4f,
+BG_GREEN = 1.0f,
+BG_OPACITY = 0.0f;
+
+
+
+// "Taekshi" AKA Leek - credit to artists of the Kagame Oni series (https://aooni.fandom.com/bjn/wiki/Leek)
+// Credit to https://kenney.nl/assets/tiny-town for the Tilemap 
+// Credit to me for the music
+
 unsigned int LEVELONE_DATA[] =
 {
-    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    3, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
-    3, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2,
-    3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2
+    48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    48, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    48, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2
 };
 
 LevelOne::~LevelOne()
 {
     delete [] m_state.enemies;
-    delete    m_state.player;
-    delete    m_state.map;
+    delete m_state.player;
+    delete m_state.map;
     Mix_FreeChunk(m_state.jump_sfx);
     Mix_FreeMusic(m_state.bgm);
 }
@@ -29,27 +40,28 @@ void LevelOne::initialise()
 {
     m_state.next_scene_id = -1;
     
-    GLuint map_texture_id = Utility::load_texture("assets/tileset.png");
-    m_state.map = new Map(LEVEL_WIDTH, LEVEL_HEIGHT, LEVELONE_DATA, map_texture_id, 1.0f, 4, 1);
+    GLuint map_texture_id = Utility::load_texture("assets/tilemap.png");
+    m_state.map = new Map(LEVEL_WIDTH, LEVEL_HEIGHT, LEVELONE_DATA, map_texture_id, 1.0f, 12, 11);
+    glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
     
     // Code from main.cpp's initialise()
     /**
      George's Stuff
      */
-    // Existing
+    // Existingleset
     m_state.player = new Entity();
     m_state.player->set_entity_type(PLAYER);
     m_state.player->set_position(glm::vec3(5.0f, 0.0f, 0.0f));
     m_state.player->set_movement(glm::vec3(0.0f));
     m_state.player->m_speed = 2.5f;
     m_state.player->set_acceleration(glm::vec3(0.0f, -9.81f, 0.0f));
-    m_state.player->m_texture_id = Utility::load_texture("assets/george_0.png");
+    m_state.player->m_texture_id = Utility::load_texture("assets/taekshi.png");
     
     // Walking
-    m_state.player->m_walking[m_state.player->LEFT]  = new int[4] { 1, 5, 9,  13 };
-    m_state.player->m_walking[m_state.player->RIGHT] = new int[4] { 3, 7, 11, 15 };
-    m_state.player->m_walking[m_state.player->UP]    = new int[4] { 2, 6, 10, 14 };
-    m_state.player->m_walking[m_state.player->DOWN]  = new int[4] { 0, 4, 8,  12 };
+    m_state.player->m_walking[m_state.player->LEFT] = new int[4] { 4, 5, 6, 7 };
+    m_state.player->m_walking[m_state.player->RIGHT] = new int[4] { 8, 9, 10, 11 };
+    m_state.player->m_walking[m_state.player->UP] = new int[4] { 12, 13, 14, 15 };
+    m_state.player->m_walking[m_state.player->DOWN] = new int[4] { 0, 1, 2, 3 };
 
     m_state.player->m_animation_indices = m_state.player->m_walking[m_state.player->RIGHT];  // start George looking left
     m_state.player->m_animation_frames = 4;
@@ -59,13 +71,15 @@ void LevelOne::initialise()
     m_state.player->m_animation_rows   = 4;
     m_state.player->set_height(0.8f);
     m_state.player->set_width(0.8f);
+
+    // Background
+    glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
     
     // Jumping
     m_state.player->m_jumping_power = 5.0f;
     
-    /**
-     Enemies' stuff */
-    GLuint enemy_texture_id = Utility::load_texture("assets/soph.png");
+    // Can't get rid of this code either...
+    GLuint enemy_texture_id = Utility::load_texture("assets/taekshi.png");
     
     m_state.enemies = new Entity[ENEMY_COUNT];
     m_state.enemies[0].set_entity_type(ENEMY);
